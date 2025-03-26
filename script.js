51554533d63f5dd339bd026e1f0a3b9e5ebb5ef9
Halla98 <hala.saad001@gmail.com>
2025-03-26 22:13:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ const img = new Image();
 img.src = "design.png"; // تأكد من المسار الصحيح للصورة
 
 img.onload = function () {
-    // ضبط حجم الـ canvas بناءً على حجم الصورة الأصلية
+    console.log("الصورة تم تحميلها بنجاح");
     canvas.width = img.width * 0.85;  // تكبير بسيط
     canvas.height = img.height * 0.85;
-
-    // رسم الصورة داخل الـ canvas
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 };
+
+function drawCanvas(name = "") {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0); // رسم الصورة
 
     if (name) {
         ctx.font = "30px 'Tajawal', sans-serif";  // تكبير الخط إلى 30px
@@ -38,7 +40,7 @@ addNameBtn.addEventListener("click", function () {
 // تحميل الصورة بعد إضافة الاسم
 downloadBtn.addEventListener("click", function () {
     const link = document.createElement('a');
-    link.download = 'eidcardsnc';  // اسم الصورة عند تحميلها
-    link.href = canvas.toDataURL("png");
+    link.download = 'eidcardsnc.png';  // اسم الصورة عند تحميلها
+    link.href = canvas.toDataURL("image/png");
     link.click();
 });
