e0016c3f94fc4e2ef437a5e29068542bf11233b0
Halla98 <hala.saad001@gmail.com>
2025-03-26 22:09:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ img.onload = function () {
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 };
 
-function drawCanvas(name = "") {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);  // مسح المحتوى القديم
-    ctx.drawImage(img, 0, 0); // رسم الصورة في الخلفية
-
     if (name) {
         ctx.font = "30px 'Tajawal', sans-serif";  // تكبير الخط إلى 30px
         ctx.fillStyle = "#2BB673";  // اللون الأخضر للاسم
@@ -42,7 +38,7 @@ addNameBtn.addEventListener("click", function () {
 // تحميل الصورة بعد إضافة الاسم
 downloadBtn.addEventListener("click", function () {
     const link = document.createElement('a');
-    link.download = 'design_with_name.png';  // اسم الصورة عند تحميلها
-    link.href = canvas.toDataURL("image/png");
+    link.download = 'eidcardsnc';  // اسم الصورة عند تحميلها
+    link.href = canvas.toDataURL("png");
     link.click();
 });
